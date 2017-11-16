c31d91b036853dfecc8602778cbf9acfc25cb71e
wanliyunyan <88527712pan@163.com>
2017-11-16 09:52:54 +0800

    delete useless file

--- src/routes/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-function default_1() {
-    return <div>404 Not Found</div>;
-}
-exports.default = default_1;
-//# sourceMappingURL=NotFound.jsx.map
